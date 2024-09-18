0499f361f4899bf4c9fe1738b5eac18ff50fcf1e
funnycat53 <patriks.jkb@gmail.com>
2024-09-18 11:16:07 +0300

    darbs

--- tests.py
+++ tests.py
@@ -4,24 +4,49 @@ class Cilveks:
         self.sex= dzimums
         self.age= vecums
     def dzimsanasdiena(self):
-            self.age +=1
+           self.age +=1
+           self.BazarsParSevi()
+
+    def mainit_vardu(self, jaunais_vards):
+            self.name = jaunais_vards
+            self.BazarsParSevi()
+
+    def mainit_dzimumu(self, jaunais_dzimums = ""):
+            if jaunais_dzimums == "":
+                if self.sex == "s":
+                    self.sex = "v"
+                else:
+                 self.sex = "s"
+            else:
+                self.dzimums = jaunais_dzimums
+
+
     def BazarsParSevi(self):
-        if self.sex == "S":
+        if self.sex == "s":
             dzimums = "sieviete"
-        elif self.sex == "V":
-            dzimums = "vīrietis"
+        elif self.sex == "v":
+             dzimums = "vīrietis"
         else:
              dzimums = self.sex
-        
+             
         if self.name == "P":
              vards = "Patriks"
         else:
              vards = self.name     
+              
         print("Sveiki, mani sauc {}, man ir {} gadi. Es esmu {}.".format(vards, self.age, dzimums))
 
-persona = Cilveks("P", "V", 18)
-persona1 = Cilveks("Anna", "S", 23)
 
+class Sieviete(Cilveks):
+     def __init__(self, vards, hair_color, vecums):
+          super().__init__(vards, "s", vecums)
+          self.matu_krasa = hair_color
+          self.BazarsParSevi()
+
+def BazarsParSevi(self):
+     super().BazarsParSevi()
+     print("Mana matu krāsa ir", )
+
+persona = Cilveks("Patriks", "V", 18)
 
 persona.BazarsParSevi()
-
